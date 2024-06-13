d2a7462e7141041baf1cd4dd6ec715cc4a72d020
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-06-13 12:35:31 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
-// import basicSsl from '@vitejs/plugin-basic-ssl';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     // Uncomment this line if you want to expose your dev server and access it from the devices
     // in the same network.
-    // host: true,
+     host: true,
   },
 });
 
